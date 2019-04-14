c4d9f711b7f9adf9f18a06eea4849cb714509465
Sergey Bolshakov <spooky5ya@yandex.ru>
2019-04-15 01:31:42 +0300

    ъъ ъъ

--- app/src/main/java/com/example/practice2/ListViewAdapter.java
+++ app/src/main/java/com/example/practice2/ListViewAdapter.java
@@ -32,7 +32,8 @@ public class ListViewAdapter extends BaseAdapter
     }
 
     @Override
-    public Object getItem(int position) {
+    public Object getItem(int position)
+    {
         return null;
     }
 
@@ -48,8 +49,7 @@ public class ListViewAdapter extends BaseAdapter
         // Declare Variables
         TextView name;
 
-        inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View itemView = inflater.inflate(R.layout.listview_item, parent, false);
         // Get the position
@@ -62,10 +62,12 @@ public class ListViewAdapter extends BaseAdapter
         final ImageView graphic = (ImageView) itemView.findViewById(R.id.graphic);
 
         if(data.get(position).get("graphic") != null)
+        {
             Glide.with(context).
                     load("https://raw.githubusercontent.com/wesleywerner/ancient-tech/02decf875616dd9692b31658d92e64a20d99f816/src/images/tech/" + data.get(position).
                             get("graphic")).
                     into(graphic);
+        }
 
         // Capture position and set results to the TextViews
         name.setText(resultp.get("name"));
--- app/src/main/java/com/example/practice2/PageFragment.java
+++ app/src/main/java/com/example/practice2/PageFragment.java
@@ -35,27 +35,40 @@ public class PageFragment extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         View view = inflater.inflate(R.layout.fragment, container, false);
 
         ImageView imageGraphic = view.findViewById(R.id.graphic);
 
         if(getArguments().getString("graphic") != null)
+        {
             graphic = getArguments().getString("graphic");
+        }
+
         if(getArguments().getString("helptext") != null)
+        {
             helptext = getArguments().getString("helptext");
+        }
 
         if(graphic != null)
+        {
             Glide.with(this).
                     load("https://raw.githubusercontent.com/wesleywerner/ancient-tech/02decf875616dd9692b31658d92e64a20d99f816/src/images/tech/" + graphic).
                     into(imageGraphic);
+        }
 
         TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
+
         if(helptext != null)
+        {
             tvPage.setText(helptext);
+        }
+
         else
-            tvPage.setText("No description found");
+        {
+            tvPage.setText("Описание отсутствует.");
+        }
 
         return view;
     }
